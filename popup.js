4d824253afc97bed933e29d38c4b22782e49180d
Frank Xiao <frank.young.xiao@gmail.com>
2015-03-07 15:38:51 -0800

    Add basic back/forward navigation.

--- popup.js
+++ popup.js
@@ -2,16 +2,60 @@
 var waQueryURL = 'http://www.wolframalpha.com/input/?i=';
 
 // Avoid typing 'getElementById()'.
-var $ = function(id)
-{
+var $ = function(id) {
   return document.getElementById(id);
+}
+
+// Go back to the previous result.
+function goBack() {
+  if (localStorage.backStack) {
+    var backStackArray = JSON.parse(localStorage.backStack);
+    // The top of backStack is the query of the current page. Check that
+    // we can go back, which we do popping the current page from backStack
+    // and pushing it to forwardStack.
+    if (backStackArray.length > 1) {
+      var prevQuery = backStackArray.pop();
+      localStorage.backStack = JSON.stringify(backStackArray);
+      if (!localStorage.forwardStack) {
+        localStorage.forwardStack = JSON.stringify([]);
+      }
+      var forwardStackArray = JSON.parse(localStorage.forwardStack);
+      forwardStackArray.push(prevQuery);
+      localStorage.forwardStack = JSON.stringify(forwardStackArray);
+      $('userQuery').value = backStackArray[backStackArray.length-1];
+      queryWA(false);
+    }
+  }
+}
+
+// Go forward to the next result.
+function goForward() {
+  if (localStorage.forwardStack) {
+    var forwardStackArray = JSON.parse(localStorage.forwardStack);
+    // The top of forwardStack is the query (if it exists) that comes after
+    // the one on the current page. Check that we can go forward, which we do 
+    // by popping the next query from forwardStack and pushing it to backStack.
+    if (forwardStackArray.length > 0) {
+      var nextQuery = forwardStackArray.pop();
+      localStorage.forwardStack = JSON.stringify(forwardStackArray);
+      if (!localStorage.backStack) {
+        localStorage.backStack = JSON.stringify([]);
+      }
+      var backStackArray = JSON.parse(localStorage.backStack);
+      backStackArray.push(nextQuery);
+      localStorage.backStack = JSON.stringify(backStackArray);
+      $('userQuery').value = nextQuery;
+      queryWA(false);
+    }
+  }
 }
 
 // Clear the child nodes of parent.
 function clear(parent)
 {
-  while (parent.firstChild)
+  while (parent.firstChild) {
     parent.removeChild(parent.firstChild);
+  }
 }
 
 // Clear the popup display.
@@ -57,8 +101,21 @@ function loadPrev()
 }
 
 // Function that does all of the work of querying WolframAlpha.
-function queryWA()
+function queryWA(addNewState)
 {
+  // Save the query for history navigation only if this wasn't called from the
+  // Back or Forward buttons.
+  if (addNewState) {
+    if (!localStorage.backStack) {
+      localStorage.backStack = JSON.stringify([]);
+    }
+    if ($('userQuery').value) {
+      console.log($('userQuery').value);
+      var backStackArray = JSON.parse(localStorage.backStack);
+      backStackArray.push(String($('userQuery').value));
+      localStorage.backStack = JSON.stringify(backStackArray);
+    }
+  }
   queryURL = waQueryURL + encodeURIComponent($('userQuery').value);
   var req = new XMLHttpRequest();
   req.addEventListener('loadstart', showProgress, false);
@@ -103,14 +160,13 @@ function queryWA()
       link.setAttribute('target', '_blank');
       link.innerHTML = 'Link to WolframAlpha Result Page';
       responseLink.appendChild(link);
+      localStorage.prevSearch = $('userQuery').value;
       localStorage.prevResponseHTML = wolframAlphaResponses.innerHTML;
-      localStorage.prevAlertHTML = alertNotice.innerHTML;
       localStorage.prevLinkHTML = responseLink.innerHTML;
+      localStorage.prevAlertHTML = alertNotice.innerHTML;
     }
   };
   req.send(null);
-  localStorage.prevSearch = $('userQuery').value;
-  console.log('saved ' + localStorage.prevSearch);
 }
 
 // Execution entry point.
@@ -120,7 +176,13 @@ document.addEventListener('DOMContentLoaded',
     loadPrev();
     $('userQuery').addEventListener('keypress', 
       function () { 
-        if (event.keyCode == 13) { event.preventDefault(); queryWA(); }
+        if (event.keyCode == 13) { 
+          event.preventDefault(); 
+          queryWA(true); 
+        }
       });
+
+    document.getElementById("backButton").addEventListener("click", goBack);
+    document.getElementById("forwardButton").addEventListener("click", goForward);
   }
 );
