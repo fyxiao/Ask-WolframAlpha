d47a360fe20f286f20247a5b812ad0942d814ee9
Frank Xiao <frank.young.xiao@gmail.com>
2015-03-08 17:14:57 -0700

    Rearrange buttons and textbox and fix the sizing of those elements.

--- popup.js
+++ popup.js
@@ -182,8 +182,8 @@ document.addEventListener('DOMContentLoaded',
         }
       });
 
-    document.getElementById("backButton").addEventListener("click", goBack);
-    document.getElementById("forwardButton").addEventListener("click", goForward);
+    document.getElementById('backButton').addEventListener('click', goBack);
+    document.getElementById('forwardButton').addEventListener('click', goForward);
     localStorage.clear();
   }
 );
